9a79094c9baea9dab3181d79b23c2624961ebe50
ReagentKing <mpkinealy@outlook.com>
2015-12-13 00:23:30 -0600

    Attempted to fix EOF reading. Didn't work

--- src/Linter.java
+++ src/Linter.java
@@ -10,12 +10,13 @@ public class Linter {
 			//creates a scanner object to parse the file
 			Scanner tokens = new Scanner(file);
 			//gets the first line to check
-			String ln = tokens.nextLine();
-			String testln;
+			//String ln = tokens.nextLine();
+			String ln;
 			//instantiates the counter
 			int lineNum = 1;
 			//loop to check the file
-			while (ln != null){
+			while (tokens.hasNextLine()){
+				ln = tokens.nextLine();
 				if(checkSemi(ln)){
 					//if the line should end in a ; and doesn't prints message
 					System.out.println("Line " + lineNum + ". Line should end in a semicolon. It does not.");
@@ -51,20 +52,23 @@ public class Linter {
 					System.out.println("Line " + lineNum + ". Use only one statement per line.");
 				}
 				//checks to see if there is a next line
-				try{
-					testln = tokens.nextLine();
-					System.out.println("Next Line: " + testln);
-				}
+				//try{
+				//	testln = tokens.nextLine();
+					//System.out.println("Next Line: " + testln);
+				//}
 				//catch Exception when next line doesn't exist
-				catch (NoSuchElementException e){
-					if(!checkEOF(ln)){
-						System.out.println("Line " + lineNum + ". File "+ args[0] + " should end with a newline character.");
-					}
-					break;
-				}
+				//catch (NoSuchElementException e){
+				//if(!checkEOF(ln)){
+					//System.out.println("Line " + lineNum + ". File "+ args[0] + " should end with a newline character.");
+				//}
 				//increment counter
-				ln=testln;
+				System.out.println("Line: " + ln);
+				//ln=tokens.nextLine();
 				lineNum++;
+			}
+			ln=tokens.next();
+			if(!checkEOF(ln)){
+				System.out.println("Line " + lineNum + ". File "+ args[0] + " should end with a newline character.");
 			}
 			//close the scanner
 			tokens.close();
